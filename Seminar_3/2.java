6d4eb3144a58bde31087a2e6ff085dcecf03d313
Denis Rezaykin <drezay@mail.ru>
2022-12-15 23:29:37 +0300

    HW 3, 4,6

---
+++ Seminar_3/2.java
@@ -0,0 +1,19 @@
+package Seminar_3;
+
+//Дан массив целых чисел (List<Integer>) удалить из него целые числа.
+//Посмотрите на метод removeAll
+//(можно сохранить все четные значения в другой список
+// и передать его в removeAll или используйте итератор
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Task2 {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(List.of(1, 2, 6, 2, 2, 2, 2, 5, 6, 7));
+        nums.removeIf(cur -> cur % 2 == 0);
+        System.out.println(nums);
+    }
+}
